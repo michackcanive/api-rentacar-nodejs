07022c8e8417c0a569fe370421b40fc6bf1e0838
Michak Canive <mechackantonio@gmail.com>
2022-04-30 02:47:01 +0100

    typeorm end migrations applied

---
+++ src/database/migrations/1651279366239-CreateCategories.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategories1651279366239 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "categories",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "discricao",
+            type: "varchar",
+          },
+          {
+            name: "create_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories");
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import { router } from "./routes";
+import "./database"
 
 const app = express();
 app.use(express.json());
 app.use(router)
 
 
+
 ///////////////////////////////////////////
 app.listen(3333, () => {
   console.log(
-    " hostname: localhost:3333 😀", "\n servidor iniciado proximo nivel 🚀"
+    `hostname: localhost:${process.env.PORTS} 😀`, "\nservidor iniciado proximo nivel 🚀"
   );
 });
